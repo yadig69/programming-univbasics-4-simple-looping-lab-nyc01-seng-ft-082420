b37c62b7f54e70682526491f01f2f2ff4626246e
Gregory Singleton <yadig69design@gmail.com>
2020-08-16 22:34:26 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -14,3 +14,21 @@ def loop_message_n_times(message, limit)
     count += 1
   end
 end
+
+def output_array(array)
+  count = 0
+  while count < array.length do
+    puts array[count]
+    count += 1
+  end
+end
+
+def return_string_array(array)
+  count = 0
+  new_array = []
+  while count < array.length do
+    array[count] = array[count].to_s
+    count += 1
+  end
+  array
+end
