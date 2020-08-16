6f00dacb945cfdc14913f393b80591ab9e407da9
Gregory Singleton <yadig69design@gmail.com>
2020-08-16 22:06:51 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,8 +1,8 @@
 # Write your methods here
-def loops_message_five_times(massage)
-  count = 0 
+def loop_message_five_times(message)
+  count = 0
   while count < 5 do
-    puts massage
-  count += 1 
-end
+    puts message
+    count += 1
+  end
 end
